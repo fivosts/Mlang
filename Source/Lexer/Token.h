bb489645cc717df6aa7e5ff59695a1f0ba04738f
fivosts <fivos_ts@hotmail.com>
2019-10-13 20:48:30 +0100

    Update on cmakelists and tokenclass

--- Source/Lexer/Token.h
+++ Source/Lexer/Token.h
@@ -3,35 +3,43 @@
 
 #include <string>
 
+enum class TokenType
+{
+
+	IMPORT,			// #import module
+	DEFINE,			// #define target Pytorch
+	
+	IDENTIFIER, 	// identifier = | {
+
+	LENOF,			// len(layer1)
+	ENDL,			// '\n'
+	QUOTE,			// '\"'
+	PLUS,			// '+'
+
+	STR_LITERAL,	// names
+	INT_LITERAL,	// integer values
+	BOOL_LITERAL,	// True/False 1/0
+
+	LBRA,			// '{'
+	RBRA,			// '}'
+	LPAR,			// '('
+	RPAR,			// ')'
+	LSBR,			// '['
+	RSBR,			// ']'
+
+	END,			// EOF
+	INVALID			// Unk token
+};
+
+template<typename T>
 class Token
 {
 public:
-	enum class TokenType{
+	Token() = default;
+	~Token() = default;
 
-		IMPORT,			// #import module
-		DEFINE,			// #define target Pytorch
-		
-		IDENTIFIER, 	// identifier = | {
-
-		LENOF,			// len(layer1)
-		ENDL,			// '\n'
-		QUOTE,			// '\"'
-
-		STR_LITERAL,	// names
-		INT_LITERAL,	// integer values
-		BOOL_LITERAL,	// True/False 1/0
-
-		LBRA,			// '{'
-		RBRA,			// '}'
-		PLUS,			// '+'
-		LPAR,			// '('
-		RPAR,			// ')'
-		LSBR,			// '['
-		RSBR,			// ']'
-
-		EOF,			// End of file
-		INVALID			// Unk token
-	}
+private:
+	T token;
 };
 
 #endif

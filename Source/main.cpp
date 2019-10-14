52ae7c02bead047be8880e2aac453fa4854e6cf4
fivosts <fivos_ts@hotmail.com>
2019-10-14 22:09:38 +0100

    Draft of tests in main
    Change-Id: I489623a7dbdb1a554a33cdbe3d8bae6d3a610262

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,18 +1,40 @@
-#include "ASTNode.h"
-#include "Attribute.h"
-#include "Block.h"
-#include "Expression.h"
-#include "Import.h"
-#include "Layer.h"
-#include "LayerProperties.h"
-#include "LengthOf.h"
-#include "Model.h"
-#include "ModelProperties.h"
-#include "Params.h"
+// #include "ASTNode.h"
+// #include "Attribute.h"
+// #include "Block.h"
+// #include "Expression.h"
+// #include "Import.h"
+// #include "Layer.h"
+// #include "LayerProperties.h"
+// #include "LengthOf.h"
+// #include "Model.h"
+// #include "ModelProperties.h"
+// #include "Params.h"
+#include "Scanner.h"
+#include "Token.h"
+#include <iostream>
+
+typedef Token::TokenType type;
 
 int main(){
+	std::string line;
+	Scanner *a = new Scanner("/home/foitsi01/MLog/Models/ase_model.mlog");
+	// std::ifstream a;
+	// a->open("/home/foitsi01/MLog/Models/ase_model.mlog");
 
-	MLParams *test = new InFeatures<int>();
+	// auto b = a->getChar();
+
+	// std::cout << b <<std::endl;
+
+	Token t(Token::TokenType::IMPORT);
+
+	if (t.getToken() == type::IMPORT){
+		std::cout << "Yay!\n";
+	}
+
+	// while(b != EOF){
+	// 	std::cout << (char)b;
+	// 	b = a->getChar();
+	// }
 
 	return 0;
 }

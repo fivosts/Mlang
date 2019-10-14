4a1db4b093e47917b2cbd16686cc5ebbc6b6dff4
fivosts <fivos_ts@hotmail.com>
2019-10-14 21:55:08 +0100

    Complete scanner
    Change-Id: Ic5ce1b97443c687ec5b300b6dbc5df44932d52a9

--- Source/Lexer/Scanner.h
+++ Source/Lexer/Scanner.h
@@ -3,10 +3,17 @@
 
 #include <fstream>
 #include <iostream>
+#include <functional>
 
 class Scanner : public std::ifstream
 {
 public:
+
+    struct Position{
+        int line;
+        int column;
+    };
+
     Scanner() = delete;
     Scanner(std::string filePath)
     {
@@ -14,7 +21,7 @@ public:
         
         try
         {this->open(filePath);}
-        
+
         catch (const std::ifstream::failure &e)
         {/*TODO Send Error report*/}
 
@@ -25,10 +32,34 @@ public:
         this->close();
     }
 
-    virtual int getChar(){
-        try                                     {       return this->get(); }
-        catch(const std::ifstream::failure &e)  {       return EOF;         }
+    virtual char getChar(){
+        try                                     {   return updatePosition(this->get()); }
+        catch(const std::ifstream::failure &e)  {   return EOF;                         }
     }
+
+    virtual char peekChar(){
+        try                                     {       return this->peek();    }
+        catch(const std::ifstream::failure &e)  {       return EOF;             }
+    }
+
+private:
+    virtual char updatePosition(char c){
+
+        if (c == '\n')
+        {
+            ++pos.line;     pos.column = 1;
+        }
+        else
+            ++pos.column;
+        return c;
+    }
+
+    virtual const Position &getCurrentPosition(){
+        return pos;
+    }
+
+private:
+    Position pos{0, 0};
 
 };
 

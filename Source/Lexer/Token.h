fcff810f4d2f320ec2ba2c6b46a0e960f8e99cd0
fivosts <fivos_ts@hotmail.com>
2019-10-14 22:08:50 +0100

    Refactor Token class
    Change-Id: I01652d17f6acd663568c0e71413ff510685b7c02

--- Source/Lexer/Token.h
+++ Source/Lexer/Token.h
@@ -3,43 +3,47 @@
 
 #include <string>
 
-enum class TokenType
-{
-
-	IMPORT,			// #import module
-	DEFINE,			// #define target Pytorch
-	
-	IDENTIFIER, 	// identifier = | {
-
-	LENOF,			// len(layer1)
-	ENDL,			// '\n'
-	QUOTE,			// '\"'
-	PLUS,			// '+'
-
-	STR_LITERAL,	// names
-	INT_LITERAL,	// integer values
-	BOOL_LITERAL,	// True/False 1/0
-
-	LBRA,			// '{'
-	RBRA,			// '}'
-	LPAR,			// '('
-	RPAR,			// ')'
-	LSBR,			// '['
-	RSBR,			// ']'
-
-	END,			// EOF
-	INVALID			// Unk token
-};
-
-template<typename T>
 class Token
 {
 public:
-	Token() = default;
+	enum class TokenType
+	{
+
+		IMPORT,			// #import module
+		DEFINE,			// #define target Pytorch
+		
+		IDENTIFIER, 	// identifier = | {
+
+		LENOF,			// len(layer1)
+		ENDL,			// '\n'
+		QUOTE,			// '\"'
+		PLUS,			// '+'
+
+		STR_LITERAL,	// names
+		INT_LITERAL,	// integer values
+		BOOL_LITERAL,	// True/False 1/0
+
+		LBRA,			// '{'
+		RBRA,			// '}'
+		LPAR,			// '('
+		RPAR,			// ')'
+		LSBR,			// '['
+		RSBR,			// ']'
+
+		END,			// EOF
+		INVALID			// Unk token
+	};
+
+	Token() = delete;
+	Token(TokenType t) : token(t) {}
 	~Token() = default;
 
+	const TokenType &getToken() const{
+		return token;
+	}
+
 private:
-	T token;
+	TokenType token;
 };
 
 #endif

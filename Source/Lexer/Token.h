5e01aa9a741e72a7ffed926269f54cebd8048f47
fivosts <fivos_ts@hotmail.com>
2019-10-18 21:08:56 +0100

    Edit TokenType enum

--- Source/Lexer/Token.h
+++ Source/Lexer/Token.h
@@ -6,32 +6,33 @@
 class Token
 {
 public:
-	enum class TokenType
+	enum class TokenType : std::string
 	{
 
-		IMPORT,			// #import module
-		DEFINE,			// #define target Pytorch
+		IMPORT				:		"IMPORT",			// #import module
+		DEFINE				:		"DEFINE",			// #define target Pytorch
 		
-		IDENTIFIER, 	// identifier = | {
+		IDENTIFIER			:		"IDENTIFIER", 		// identifier = | {
 
-		LENOF,			// len(layer1)
-		ENDL,			// '\n'
-		QUOTE,			// '\"'
-		PLUS,			// '+'
+		LENOF				:		"LENOF",			// len(layer1)
+		NEWLINE				:		"NEWLINE",				// '\n'
+		QUOTE				:		"QUOTE",			// '\"'
+		PLUS				:		"PLUS",				// '+'
+		ASSIGN				:		"ASSIGN",			// '='
 
-		STR_LITERAL,	// names
-		INT_LITERAL,	// integer values
-		BOOL_LITERAL,	// True/False 1/0
+		STR_LITERAL			:		"STR_LITERAL",		// names
+		INT_LITERAL			:		"INT_LITERAL",		// integer values
+		BOOL_LITERAL		:		"BOOL_LITERAL",		// True/False 1/0
 
-		LBRA,			// '{'
-		RBRA,			// '}'
-		LPAR,			// '('
-		RPAR,			// ')'
-		LSBR,			// '['
-		RSBR,			// ']'
+		LBRA				:		"LBRA",				// '{'
+		RBRA				:		"RBRA",				// '}'
+		LPAR				:		"LPAR",				// '('
+		RPAR				:		"RPAR",				// ')'
+		LSBR				:		"LSBR",				// '['
+		RSBR				:		"RSBR",				// ']'
 
-		END,			// EOF
-		INVALID			// Unk token
+		END					:		"END",				// EOF
+		INVALID				:		"INVALID"			// Unk token
 	};
 
 	Token() = delete;

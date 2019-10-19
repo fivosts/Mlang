47df51fffe77cae3358e5485a8c01c3b6771706f
fivosts <fivos_ts@hotmail.com>
2019-10-19 12:18:37 +0100

    Add util functions

--- Source/Util/Util.h
+++ Source/Util/Util.h
@@ -1,4 +1,18 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+bool isDigit(char &c)
+{
+	return (c <= (char)('9') 
+			&& c >= (char)('0'));
+}
+
+bool isLetter(char &c)
+{
+	return (c >= (char)'A' 
+			&& c <= (char)'Z' 
+			&& c >= (char)'a' 
+			&& c <= (char)'z');
+}
+
 #endif

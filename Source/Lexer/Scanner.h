0dcfa0a858edd1631c0b42a300afab9c2f2f3008
fivosts <fivos_ts@hotmail.com>
2019-10-19 12:19:09 +0100

    Remove const keyword

--- Source/Lexer/Scanner.h
+++ Source/Lexer/Scanner.h
@@ -32,7 +32,7 @@ public:
         this->close();
     }
 
-    virtual const char nextChar()
+    virtual char nextChar()
     {
         try                                     {   return updatePosition(this->get()); }
         catch(const std::ifstream::failure &e)  {   return EOF;                         }

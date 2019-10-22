a8db31e9e4ef01fa82eb69b783369a6f220c5304
fivosts <fivos_ts@hotmail.com>
2019-10-22 20:11:00 +0100

    Add test files for lexer, scanner. Modify tests

---
+++ Test/Lexer/Lexer.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+namespace MLogTest
+{
+    TEST(ContextTest, ContextConstructora)
+    {
+
+        ASSERT_FALSE(1==0);
+    }
+}
+
---
+++ Test/Scanner/Scanner.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+namespace MLogTest
+{
+    TEST(ContextTest, ContextConstructor)
+    {
+
+        ASSERT_FALSE(1==0);
+    }
+}
+

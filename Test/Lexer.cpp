bace1c852c42b76be5362d72857cc41f76a349fd
fivosts <fivos_ts@hotmail.com>
2019-10-25 18:17:28 +0100

    Insert new lexer test
    Change-Id: I5ecb7c43d6c2a0c3f6272a6ce6f244dff4fe47fe

--- Test/Lexer.cpp
+++ Test/Lexer.cpp
@@ -67,5 +67,81 @@ namespace MLogTest
 
         delete sc;
     }
+
+    TEST(LexerTest, BadString1)
+    {
+        sc = new Scanner(FULL_PATH("bad_string1.mlog"));
+        Lexer lex(sc);
+        std::unordered_set<Token::TokenType> TokenBag;
+        
+        Token curr = lex.nextToken();
+        TokenBag.insert(curr.getToken());
+        while(curr.getToken() != Token::TokenType::END)
+        {
+            curr = lex.nextToken();
+            TokenBag.insert(curr.getToken());
+        }
+        ASSERT_TRUE(TokenBag.find(Token::TokenType::INVALID) != TokenBag.end());
+        ASSERT_FALSE(TokenBag.find(Token::TokenType::END) == TokenBag.end());
+
+        delete sc;
+    }
+
+    TEST(LexerTest, BadString2)
+    {
+        sc = new Scanner(FULL_PATH("bad_string2.mlog"));
+        Lexer lex(sc);
+        std::unordered_set<Token::TokenType> TokenBag;
+        
+        Token curr = lex.nextToken();
+        TokenBag.insert(curr.getToken());
+        while(curr.getToken() != Token::TokenType::END)
+        {
+            curr = lex.nextToken();
+            TokenBag.insert(curr.getToken());
+        }
+        ASSERT_TRUE(TokenBag.find(Token::TokenType::INVALID) != TokenBag.end());
+        ASSERT_FALSE(TokenBag.find(Token::TokenType::END) == TokenBag.end());
+
+        delete sc;
+    }
+
+    TEST(LexerTest, BadString3)
+    {
+        sc = new Scanner(FULL_PATH("bad_string3.mlog"));
+        Lexer lex(sc);
+        std::unordered_set<Token::TokenType> TokenBag;
+        
+        Token curr = lex.nextToken();
+        TokenBag.insert(curr.getToken());
+        while(curr.getToken() != Token::TokenType::END)
+        {
+            curr = lex.nextToken();
+            TokenBag.insert(curr.getToken());
+        }
+        ASSERT_TRUE(TokenBag.find(Token::TokenType::INVALID) != TokenBag.end());
+        ASSERT_FALSE(TokenBag.find(Token::TokenType::END) == TokenBag.end());
+
+        delete sc;
+    }
+
+    TEST(LexerTest, BadString4)
+    {
+        sc = new Scanner(FULL_PATH("bad_string4.mlog"));
+        Lexer lex(sc);
+        std::unordered_set<Token::TokenType> TokenBag;
+        
+        Token curr = lex.nextToken();
+        TokenBag.insert(curr.getToken());
+        while(curr.getToken() != Token::TokenType::END)
+        {
+            curr = lex.nextToken();
+            TokenBag.insert(curr.getToken());
+        }
+        ASSERT_TRUE(TokenBag.find(Token::TokenType::INVALID) != TokenBag.end());
+        ASSERT_FALSE(TokenBag.find(Token::TokenType::END) == TokenBag.end());
+
+        delete sc;
+    }
 }
 

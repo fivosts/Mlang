5f26d7f61468ead4bf825c6bc0a9a1753068d6c7
fivosts <fivos_ts@hotmail.com>
2019-10-25 11:52:52 +0100

    Lexer test fails
    Change-Id: If6db2dc0b06decd0973195888d78ca6dcb85fd58

--- Source/Lexer/Lexer.h
+++ Source/Lexer/Lexer.h
@@ -13,7 +13,6 @@ public:
 
 private:
 	Scanner *sc;
-
 };
 
 #endif
--- Test/Lexer.cpp
+++ Test/Lexer.cpp
@@ -1,10 +1,71 @@
 #include "gtest/gtest.h"
+#include <unordered_set>
+
+#include "Lexer.h"
+
+#ifdef MODEL_PATH
+#define FULL_PATH(M) MODEL_PATH M
+#endif
+
+static Scanner *sc;
 
 namespace MLogTest
 {
-    TEST(LexerTest, l1)
+    TEST(LexerTest, Empty)
     {
+        sc = new Scanner(FULL_PATH("empty.mlog"));
+        Lexer lex(sc);
+        std::unordered_set<Token::TokenType> TokenBag;
+        
+        Token curr = lex.nextToken();
+        TokenBag.insert(curr.getToken());
+        while(curr.getToken() != Token::TokenType::END)
+        {
+            curr = lex.nextToken();
+            TokenBag.insert(curr.getToken());
+        }
+        ASSERT_TRUE(TokenBag.find(Token::TokenType::INVALID) == TokenBag.end() );
+        ASSERT_TRUE(TokenBag.find(Token::TokenType::END) == TokenBag.end() );
 
+        delete sc;
+    }
+
+    TEST(LexerTest, Normal)
+    {
+        sc = new Scanner(FULL_PATH("ase_model.mlog"));
+        Lexer lex(sc);
+        std::unordered_set<Token::TokenType> TokenBag;
+        
+        Token curr = lex.nextToken();
+        TokenBag.insert(curr.getToken());
+        while(curr.getToken() != Token::TokenType::END)
+        {
+            curr = lex.nextToken();
+            TokenBag.insert(curr.getToken());
+        }
+        ASSERT_TRUE(TokenBag.find(Token::TokenType::INVALID) == TokenBag.end());
+        ASSERT_TRUE(TokenBag.find(Token::TokenType::END) == TokenBag.end() );
+
+        delete sc;
+    }
+
+    TEST(LexerTest, Binary)
+    {
+        sc = new Scanner(FULL_PATH("binary.mlog"));
+        Lexer lex(sc);
+        std::unordered_set<Token::TokenType> TokenBag;
+        
+        Token curr = lex.nextToken();
+        TokenBag.insert(curr.getToken());
+        while(curr.getToken() != Token::TokenType::END)
+        {
+            curr = lex.nextToken();
+            TokenBag.insert(curr.getToken());
+        }
+        ASSERT_TRUE(TokenBag.find(Token::TokenType::INVALID) != TokenBag.end());
+        ASSERT_TRUE(TokenBag.find(Token::TokenType::END) == TokenBag.end() );
+
+        delete sc;
     }
 }
 

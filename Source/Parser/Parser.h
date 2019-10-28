1e3a6a526a567dc86f68698a0b3a1274050584b3
fivosts <fivos_ts@hotmail.com>
2019-10-28 18:32:25 +0000

    Converted pointer functions to shared pointers

--- Source/Parser/Parser.h
+++ Source/Parser/Parser.h
@@ -1,27 +1,40 @@
 #pragma once
 
+#include "Lexer.h"
+#include "Model.h"
+
+#include <queue>
+#include <memory>
+
 class Parser
 {	
 public:
 	Parser(Lexer l) : lex(l) {}
 	~Parser() = default;
 
-	bool accept(Token::TokenType &exp)
+	bool accept(Token &exp)
 	{
+		/*
+			if queue is empty, invoke lambda to fetch one from lexer
+			Otherwise load the queue
+			!!! Don't do unnecessary reads from lexer.
+			Lexer should be responsible to cut this off
+		*/
 
+		return (/*lambda*/ == exp ? true : false)
 	}
-	bool expect(Token::TokenType &exp);
+	bool expect(Token &exp);
 
-	Model parse() { return parseModel(); }
+	std::shared_ptr<Model> parse() { return parseModel(); }
 
 private:
 	Lexer lex;
-	std::queue<Token::TokenType>
+	std::queue<Token::TokenType> tokQu;
 
 private:
 	/***** Recursive descent parsing functions  *****/
-	Model parseModel();
-	Import parseImport();
-	Attribute parseAttribute();
-	Layer parseLayer();
+	std::shared_ptr<Model> parseModel();
+	std::shared_ptr<Import> parseImport();
+	std::shared_ptr<Attribute> parseAttribute();
+	std::shared_ptr<Layer> parseLayer();
 };

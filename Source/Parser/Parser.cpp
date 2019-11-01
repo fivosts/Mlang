45d6b2dc315f82a18b77f66750e3f9e9d50b1e3b
fivosts <fivos_ts@hotmail.com>
2019-11-01 19:42:51 +0000

    Parser skeleton descent functions
    Change-Id: If46ef9ffe12e0372c29359de0a6a0b65ad39a131

--- Source/Parser/Parser.cpp
+++ Source/Parser/Parser.cpp
@@ -1,2 +1,29 @@
 #include "Parser.h"
+#include "Import.h"
+// template<typename T>
+// using setPtr = std::unordered_set<std::unique_ptr<T>>;
 
+std::unique_ptr<Model> Parser::parseModel()
+{
+    return std::make_unique<Model>(Parser::parseImport(),
+                                    Parser::parseAttribute(),
+                                    Parser::parseLayer() );
+}
+
+std::unordered_set<std::unique_ptr<Import>> Parser::parseImport(){
+    // std::unordered_set<std::unique_ptr<Import>> a;
+    // return a;
+    return {};
+}
+
+std::unordered_set<std::unique_ptr<Attribute>> Parser::parseAttribute(){
+    // std::unordered_set<std::unique_ptr<Attribute>> a;
+    // return a;
+    return {};
+}
+
+std::unordered_set<std::unique_ptr<Layer>> Parser::parseLayer(){
+    // std::unordered_set<std::unique_ptr<Layer>> a;
+    // return a;
+    return {};
+}

111e65f0e54fa9ce37c6acc690a635070d96b27a
fivosts <fivos_ts@hotmail.com>
2019-11-29 14:39:57 +0000

    implement parseIdentifier function
    Change-Id: Ib0ab113de88ab29268753b457c8bde8e6b8c5ca9

--- Source/Parser/Parser.cpp
+++ Source/Parser/Parser.cpp
@@ -15,7 +15,8 @@ std::unique_ptr<Model> Parser::parseModel()
                                     parseLayer() );
 }
 
-setPtr<Import> Parser::parseImport(){
+setPtr<Import> Parser::parseImport()
+{
 
     setPtr<Import> imports;
     while(accept(TType::IMPORT))
@@ -28,21 +29,38 @@ setPtr<Import> Parser::parseImport(){
     return imports;
 }
 
-setPtr<Attribute> Parser::parseAttribute(){
+setPtr<Attribute> Parser::parseAttribute()
+{
     setPtr<Attribute> attr;
 
     while(accept(TType::DEFINE))
     {
-        expect(TType::DEINE));
-        Token defineElement = parseIdentifier(expect(TType::IDENTIFIER));
+        expect(TType::DEFINE);
+        Token defType = parseIdentifier<Attribute>(expect(TType::IDENTIFIER));
         expect(TType::NEWLINE);
     }
-
+    // new Attribute will be called here
     return attr;
 }
 
-setPtr<Layer> Parser::parseLayer(){
+setPtr<Layer> Parser::parseLayer()
+{
     // setPtr<Layer> a;
     // return a;
     return {};
+}
+
+template<typename T>
+Token parseIdentifier(Token expID)
+{
+    // if data of identifier belongs to the map of T, then we are fine
+    if (T::IDMap.find(expID.getData()) != T::IDMap.end())
+    {
+        // element found in the type's map and everything is fine
+    }
+    else
+    {
+        // We've got a parsing error because the wrong identifier was used for a specific field.
+        throw CompExcept(joinWhSpace("Parsing Error", expID.getStrPos(), "Identifier", expID.getData(), "not found for type", T::));
+    }
 }

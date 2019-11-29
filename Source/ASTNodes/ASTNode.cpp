45d7eb7124ce6a83c4667c986deed8a26dd73811
fivosts <fivos_ts@hotmail.com>
2019-11-29 18:43:01 +0000

    Extend virtual destructors
    Change-Id: Id39ab838f4203151cabff36e67f344e7f7c362ee

--- Source/ASTNodes/ASTNode.cpp
+++ Source/ASTNodes/ASTNode.cpp
@@ -1,7 +1,9 @@
 #include "ASTNode.h"
 #include "Identifier.h"
 #include "Attribute.h"
+#include "Params.h"
 
 ASTNode::~ASTNode() {};
 Identifier::~Identifier() {};
 Attribute::~Attribute() {};
+LayerParams::~LayerParams() {};

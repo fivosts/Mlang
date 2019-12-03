77cd29b6dd1319371086a4afdd6de3abcf951199
fivosts <fivos_ts@hotmail.com>
2019-12-03 19:13:47 +0000

    Extend block classes
    Change-Id: Icbf044c96571b9301e96b241b915adf0a38c019a

--- Source/ASTNode/Block.h
+++ Source/ASTNode/Block.h
@@ -5,26 +5,29 @@
 #include <unordered_set>
 
 template<typename T>
-using setParams<T> = std::unordered_set<std::unique_ptr<T>>;
+using setParams = std::unordered_set<std::unique_ptr<T>>;
 
 class ParamBlock : public ASTNode
 {
 public:
 	ParamBlock() = delete;
-	ParamBlock(setParams<LayerParams> lp) : lparams(lp) {}
+	// ParamBlock(setParams<LayerParams> lp) : lparams(lp) {}
 	~ParamBlock() = default;
 
 private:
+	//! TODO this unique ptr will nag about this line
 	setParams<LayerParams> lparams;
 };
 
-class HyperparamBlock : public ASTNode
+class HyperparamBlock : public ASTNode, public Identifier
 {
 public:
-	HyperparamBlock() = delete;
-	HyperparamBlock(setParams<HyperParams> hp) : hparams(hp) {}
+	// TODO delete
+	HyperparamBlock() = default;
+	// HyperparamBlock(setParams<HyperParams> hp) : hparams(hp) {}
 	~HyperparamBlock() = default;
 
 private:
+	//! TODO this unique ptr will nag about this line
 	setParams<HyperParams> hparams;
 };

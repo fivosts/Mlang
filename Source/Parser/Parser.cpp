cc5e274b17396b1edcc7359484f996a2d09805c7
fivosts <fivos_ts@hotmail.com>
2019-12-03 18:27:14 +0000

    Implement parseIdentifier
    Change-Id: I09009b0062d541ef924e17a4f34f2d46e88d061c

--- Source/Parser/Parser.cpp
+++ Source/Parser/Parser.cpp
@@ -12,15 +12,19 @@ typedef Token::TokenType TType;
 
 std::unique_ptr<Model> Parser::parseModel()
 {
-    return std::make_unique<Model>(parseImport(),
-                                    parseAttribute(),
-                                    parseLayer() );
+
+    /* The order of evaluation of function arguments is unspecified. */
+    /* They have to be specified explicitly */
+    setPtr<Import> tempA = parseImport();
+    setPtr<Attribute> tempB = parseAttribute();
+    setPtr<Layer> tempC = parseLayer();
+    return std::make_unique<Model>(std::move(tempA), std::move(tempB), std::move(tempC));
 }
 
 setPtr<Import> Parser::parseImport()
 {
 #ifdef PARDBG
-    printf("PARSER: parseImport()\n");
+    printf("PARSER:\t\tparseImport()\n");
 #endif
     setPtr<Import> imports;
     while(accept(TType::IMPORT))
@@ -36,7 +40,7 @@ setPtr<Import> Parser::parseImport()
 void Parser::parseNewLines()
 {
 #ifdef PARDBG
-    printf("PARSER: parseNewLines()\n");
+    printf("PARSER:\t\tparseNewLines()\n");
 #endif
     expect(TType::NEWLINE);
     while (accept(TType::NEWLINE))
@@ -49,11 +53,10 @@ void Parser::parseNewLines()
 setPtr<Attribute> Parser::parseAttribute()
 {
 #ifdef PARDBG
-    printf("PARSER: parseAttribute()\n");
+    printf("PARSER:\t\tparseAttribute()\n");
 #endif
-    setPtr<Attribute> attr;
 
-    std::cout << "Attributes\n";
+    setPtr<Attribute> attr;
     while(accept(TType::DEFINE))
     {
         expect(TType::DEFINE);
@@ -63,11 +66,11 @@ setPtr<Attribute> Parser::parseAttribute()
         //     throw CompExcept("Parsing Error: Attribute identifier not found");
         // }
         // parseIdentifier(defType);
-    std::cout << "In while\n";
 
         // This obj should be of type subclass and have constructed its members already
         // std::unique_ptr<Attribute> AttrObj = static_cast<Attribute*>(parseIdentifier(expect(TType::IDENTIFIER)));
-        Attribute* AttrObj = static_cast<Attribute*>(parseIdentifier(expect(TType::IDENTIFIER)));
+        // Attribute* AttrObj = static_cast<Attribute*>(parseIdentifier(expect(TType::IDENTIFIER)));
+        Identifier* AttrObj = parseIdentifier(expect(TType::IDENTIFIER));
 
         AttrObj->printData();
 
@@ -80,7 +83,7 @@ setPtr<Attribute> Parser::parseAttribute()
 setPtr<Layer> Parser::parseLayer()
 {
 #ifdef PARDBG
-    printf("PARSER: parseLayer()\n");
+    printf("PARSER:\t\tparseLayer()\n");
 #endif
     // setPtr<Layer> a;
     // return a;
@@ -91,58 +94,125 @@ setPtr<Layer> Parser::parseLayer()
 std::string parseStrLiteral()
 {
 #ifdef PARDBG
-    printf("PARSER: parseStrLiteral()\n");
+    printf("PARSER:\t\tparseStrLiteral()\n");
 #endif
     return "a";
 }
 
-// template<typename T>
 Identifier* Parser::parseIdentifier(Token expID)
 {
 #ifdef PARDBG
-    printf("PARSER: parseIdentifier()\n");
+    printf("PARSER:\t\tparseIdentifier()\n");
 #endif
     std::string IDName = expID.getData();
 
+    ////////////////////////
+    //! Attributes
+    ////////////////////////
     if (IDName == "network_name")
     {
         return new NetworkName(parseStrLiteral());
     }
+    else if (IDName == "backend")
+    {
+
+    }
+    else if (IDName == "target")
+    {
+
+    }
+    else if (IDname == "cuda_available")
+    {
+
+    }
+    else if (IDName == "input_tensors")
+    {
+
+    }
+    else if (IDName == "output_tensors")
+    {
+
+    }
+    ////////////////////////
+    //! LayerParams
+    ////////////////////////
+    else if (IDName == "input")
+    {
+
+    }
+    else if (IDName == "output")
+    {
+
+    }
     else if (IDName == "layer_name")
     {
-        return new LayerName(parseStrLiteral());
+
+    }
+    ////////////////////////
+    //! LSTMParams
+    ////////////////////////
+    else if (IDName == "input_size")
+    {
+
+    }
+    else if (IDName == "output_timestep")
+    {
+
+    }
+    else if (IDName == "hidden_size")
+    {
+
+    }
+    else if (IDName == "num_layers")
+    {
+
+    }
+    ////////////////////////
+    //! MLParams
+    ////////////////////////
+    else if (IDName == "in_features")
+    {
+
+    }
+    else if (IDName == "out_features")
+    {
+
+    }
+    ////////////////////////
+    //! ASTNode
+    ////////////////////////
+    else if (IDName == "hyperparam_block")
+    {
+
+    }
+    else if (IDName == "len")
+    {
+
+    }
+    ////////////////////////
+    //! Layer
+    ////////////////////////
+    else if (IDName == "LSTM")
+    {
+
+    }
+    else if (IDName == "MLP")
+    {
+
+    }
+    else if (IDName == "SIGMOID")
+    {
+
+    }
+    else
+    {
+        // crash
+        // TODO this
+        throw;
     }
 
+    // TODO this
     return nullptr;
-    // switch (IDName)
-    // {
-    //     case "network_name":
-            // That here is the field of the object
-            // that will be constructed
-        
-        // case "cuda_available":
-        //     parseBoolLiteral();
-        //     break;
-        
-        // case "backend":
-        //     parseStrLiteral();
-        //     break;
-        
-        // case "target":
-        //     parseStrLiteral();
-        //     break;
-        
-        // case "input_tensors":
-        //     parseStrArray();
-        //     break;
-        
-        // case "output_tensors":
-        //     parseStrArray();
-        //     break;
-        
-    //     default:
-    //         throw CompExcept("Parsing Error: Identifier not recognized");
-    //         break;
-    // }
+
 }
 

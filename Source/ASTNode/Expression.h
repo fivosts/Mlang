0cab64f7cf8d172daa57e71776aed4e8af8a890c
fivosts <fivos_ts@hotmail.com>
2019-12-07 18:14:52 +0000

    Added overloaded constructor on binexpr
    Change-Id: Ib187d98a634ac63b0b945be0f43e92254a4e9d93

--- Source/ASTNode/Expression.h
+++ Source/ASTNode/Expression.h
@@ -4,9 +4,10 @@ class BinExpr : public ASTNode
 {
 public:
 	BinExpr() = delete;
-	BinExpr(std::unique_ptr<BinExpr> b) : lhs(std::move(b)) {}
+	BinExpr(std::unique_ptr<BinExpr> b, std::string r) : lhs(std::move(b)), rhs(r) {}
+	BinExpr(std::string r) : rhs(r) {}
 	~BinExpr() = default;
 private:
-	std::unique_ptr<BinExpr> lhs;
+	std::unique_ptr<BinExpr> lhs = NULL;
 	std::string rhs;
 };

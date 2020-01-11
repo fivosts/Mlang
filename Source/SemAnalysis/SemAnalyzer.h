a079a1ac37e905fa01efdada1700f645eac35a0c
fivosts <fivos_ts@hotmail.com>
2020-01-11 18:01:37 +0200

    Add semantic analyzer draft

---
+++ Source/SemAnalysis/SemAnalyzer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <memory>
+
+class SemAnalyzer
+{
+public:
+	SemAnalyzer() = delete;
+	SemAnalyzer(std::shared_ptr<Model> &&m) : model(std::move(m)) {}
+
+private:
+	std::shared_ptr<Model> model = NULL;
+};

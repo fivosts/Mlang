6bda3a56a615623be8ec963c8aca31c9151ded70
fivosts <fivos_ts@hotmail.com>
2020-01-11 23:40:41 +0200

    Extend semantic analyzer class

--- Source/SemAnalysis/SemAnalyzer.h
+++ Source/SemAnalysis/SemAnalyzer.h
@@ -8,7 +8,17 @@ public:
 	SemAnalyzer() = delete;
 	SemAnalyzer(std::shared_ptr<Model> &&m) : model(std::move(m)) {}
 
-	
+	std::shared_ptr<Model> safeAnalyzeModel()
+	{
+		try 						{	AnalyzeModel();
+										return model;			}
+		catch (CompExcept& ex)		{	throw;					}
+	}
+
+private:
+	void AnalyzeModel();
+	void AnalyzeNames();
+	void AnalyzeTypes();
 
 private:
 	std::shared_ptr<Model> model = NULL;

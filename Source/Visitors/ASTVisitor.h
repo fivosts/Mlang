43d43e2b588f5e74079bac47a5e9e9850672e405
fivosts <fivos_ts@hotmail.com>
2020-01-27 17:28:56 +0000

    Extend ASTVisitor class

--- Source/Visitors/ASTVisitor.h
+++ Source/Visitors/ASTVisitor.h
@@ -1,10 +1,20 @@
 #pragma once
 
+/* Forward declaration of ast types */
+
+class Model;
+class LayerParams;
+
 class ASTVisitor
 {
 public:
 	ASTVisitor() = default;
 	virtual ~ASTVisitor() = 0;
+
+	virtual void visit(std::string /*s*/){}
+	virtual void visit(std::unique_ptr<LayerParams> lp){
+		(void)lp;
+	}
 
 protected:
 	std::shared_ptr<Model> model = NULL;

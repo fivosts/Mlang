6378920357d3ccebc84ad1ac11881e95d31cd9e2
fivosts <fivos_ts@hotmail.com>
2020-01-27 15:09:47 +0000

    Fix identation with spaces instead of tabs

--- Source/SemAnalysis/SemAnalyzer.h
+++ Source/SemAnalysis/SemAnalyzer.h
@@ -5,30 +5,30 @@
 class SemAnalyzer
 {
 public:
-	SemAnalyzer() = delete;
-	SemAnalyzer(std::shared_ptr<Model> &&m) : model(std::move(m)) {}
+    SemAnalyzer() = delete;
+    SemAnalyzer(std::shared_ptr<Model> &&m) : model(std::move(m)) {}
 
-	std::shared_ptr<Model> safeAnalyzeModel()
-	{
-		try 						{	AnalyzeModel();
-										return model;			}
-		catch (CompExcept& ex)		{	throw;					}
-	}
-
-private:
-
-	// Analyze model will call analyze name and types
-	void AnalyzeModel();
-
-	// Name Analysis in 3 aspects 
-	// Includes: Does the module exist ?
-	// Attributes: add to symbol table
-	// Layers: add declared variables to symbol table
-	// Then check if used symbols are legit.
-	// Is scope in this context meaningful ?
-	void AnalyzeNames();
-	void AnalyzeTypes();
+    std::shared_ptr<Model> safeAnalyzeModel()
+    {
+        try                         {    AnalyzeModel();
+                                        return model;            }
+        catch (CompExcept& ex)        {    throw;                    }
+    }
 
 private:
-	std::shared_ptr<Model> model = NULL;
+
+    // Analyze model will call analyze name and types
+    void AnalyzeModel();
+
+    // Name Analysis in 3 aspects 
+    // Includes: Does the module exist ?
+    // Attributes: add to symbol table
+    // Layers: add declared variables to symbol table
+    // Then check if used symbols are legit.
+    // Is scope in this context meaningful ?
+    void AnalyzeNames();
+    void AnalyzeTypes();
+
+private:
+    std::shared_ptr<Model> model = NULL;
 };

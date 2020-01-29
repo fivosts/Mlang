b7987982857f7d06250cd9ed52741f760d319348
fivosts <fivos_ts@hotmail.com>
2020-01-29 19:08:41 +0000

    Refactor and reorder virtuals for visit methods

--- Source/Visitors/ASTVisitor.h
+++ Source/Visitors/ASTVisitor.h
@@ -14,52 +14,18 @@ public:
 	ASTVisitor() = default;
 	virtual ~ASTVisitor() = 0;
 
-	virtual void visit(std::string /*s*/){
-
-	}
-
-	virtual void visit(int /*s*/){
-
-	}
-
-	virtual void visit(const std::unique_ptr<LayerParams> &lp){
-		(void)lp;
-		lp->accept(this);
-	}
-
-	virtual void visit(const std::unique_ptr<HyperParams> &hp){
-		(void)hp;
-	}
-
-	virtual void visit(const std::unique_ptr<BinExpr> &lp){
-		(void)lp;
-	}
-
+	virtual void visit(const std::unique_ptr<Model> & /*m*/) {}
+	virtual void visit(const std::unique_ptr<Import> & /*i*/) {}
+	virtual void visit(const std::unique_ptr<Attribute> & /*s*/) {}
+	virtual void visit(const std::unique_ptr<Layer> & /*l*/) {}
+	virtual void visit(const std::unique_ptr<ParamBlock> & /*pb*/) {}
+	virtual void visit(const std::unique_ptr<HyperparamBlock> & /*hpb*/) {}
+	virtual void visit(const std::unique_ptr<LayerParams> & /*lp*/) {}
+	virtual void visit(const std::unique_ptr<HyperParams> & /*hp*/) {}
+	virtual void visit(const std::unique_ptr<BinExpr> & /*be*/) {}
 	virtual void visit(const std::unique_ptr<LengthOf> & /*lo*/) {}
-
-	virtual void visit(const std::unique_ptr<Layer> &lp){
-		(void)lp;
-	}
-
-	virtual void visit(const std::unique_ptr<Model> &lp){
-		(void)lp;
-	}
-
-	virtual void visit(const std::unique_ptr<ParamBlock> &lp){
-		(void)lp;
-	}
-
-	virtual void visit(const std::unique_ptr<HyperparamBlock> &lp){
-		(void)lp;
-	}
-
-	virtual void visit(const std::unique_ptr<Import> &lp){
-		lp->accept(this);
-	}
-
-	virtual void visit(const std::unique_ptr<Attribute> &lp){
-		(void)lp;
-	}
+	virtual void visit(std::string /*s*/) {}
+	virtual void visit(int /*i*/) {}
 
 // protected:
 // 	const std::unique_ptr<Model> model = NULL;

e827171a35798b9e4b9806d968968a081b2d3789
fivosts <fivos_ts@hotmail.com>
2020-01-29 19:57:11 +0000

    Reorder ASTNode.cpp functions

--- Source/ASTNode/ASTNode.cpp
+++ Source/ASTNode/ASTNode.cpp
@@ -5,24 +5,20 @@
 #include "Layer.h"
 #include "ModelProperties.h"
 
+/*
+Pure virtual Destructors
+*/
 ASTNode::~ASTNode() {}
 Attribute::~Attribute() {}
 LayerParams::~LayerParams() {}
 HyperParams::~HyperParams() {}
 Layer::~Layer() {}
 
-void ParamBlock::accept(ASTVisitor* v)
-{
-	for (auto &lp : lparams)
-		v->visit(lp);
-}
+/*
+Visitor methods for AST Nodes
+*/
 
-void HyperparamBlock::accept(ASTVisitor* v)
-{
-	for (auto &hp : hparams)
-		v->visit(hp);
-}
-
+//! Model
 void Model::accept(ASTVisitor* v)
 {
 	for (auto &i : imp)
@@ -33,87 +29,13 @@ void Model::accept(ASTVisitor* v)
 		v->visit(l);
 }
 
-void BinExpr::accept(ASTVisitor* v)
-{
-	v->visit(lhs);
-	v->visit(rhs);
-}
-
-void LSTM::accept(ASTVisitor* v)
-{
-	v->visit(pblock);
-	v->visit(hblock);
-}
-
-void MLP::accept(ASTVisitor* v)
-{
-	v->visit(pblock);
-	v->visit(hblock);
-}
-
-void Sigmoid::accept(ASTVisitor* v)
-{
-	v->visit(pblock);
-}
-
+//! Import
 void Import::accept(ASTVisitor* v)
 {
 	v->visit(path);
 }
 
-void Input::accept(ASTVisitor* v)
-{
-	v->visit(inp);
-}
-
-void Output::accept(ASTVisitor* v)
-{
-	v->visit(outp);
-}
-
-void LayerName::accept(ASTVisitor* v)
-{
-	v->visit(name);
-}
-
-void InputSize::accept(ASTVisitor* v)
-{
-	v->visit(inSize);
-	v->visit(llength);
-}
-
-void OutputTimestep::accept(ASTVisitor* v)
-{
-	v->visit(outStep);
-}
-
-void HiddenSize::accept(ASTVisitor* v)
-{
-	v->visit(hSize);
-}
-
-void NumLayers::accept(ASTVisitor* v)
-{
-	v->visit(nLayers);
-}
-
-void InFeatures::accept(ASTVisitor* v)
-{
-	v->visit(inF);
-	v->visit(llength);
-}
-
-void OutFeatures::accept(ASTVisitor* v)
-{
-	for(auto &o : outFeat)
-		v->visit(o);
-}
-
-void LengthOf::accept(ASTVisitor* v)
-{
-	v->visit(layer);
-}
-
+//! Attribute
 void NetworkName::accept(ASTVisitor* v)
 {
 	v->visit(name);
@@ -145,3 +67,104 @@ void OutTensors::accept(ASTVisitor* v)
 	for (auto &i : outTens)
 		v->visit(i);
 }
+//!
+
+//! Layer
+void LSTM::accept(ASTVisitor* v)
+{
+	v->visit(pblock);
+	v->visit(hblock);
+}
+
+void MLP::accept(ASTVisitor* v)
+{
+	v->visit(pblock);
+	v->visit(hblock);
+}
+
+void Sigmoid::accept(ASTVisitor* v)
+{
+	v->visit(pblock);
+}
+//!
+
+//! Block
+void ParamBlock::accept(ASTVisitor* v)
+{
+	for (auto &lp : lparams)
+		v->visit(lp);
+}
+
+void HyperparamBlock::accept(ASTVisitor* v)
+{
+	for (auto &hp : hparams)
+		v->visit(hp);
+}
+//!
+
+//! LayerParams
+void Input::accept(ASTVisitor* v)
+{
+	v->visit(inp);
+}
+
+void Output::accept(ASTVisitor* v)
+{
+	v->visit(outp);
+}
+
+void LayerName::accept(ASTVisitor* v)
+{
+	v->visit(name);
+}
+//!
+
+//! LSTMParams
+void InputSize::accept(ASTVisitor* v)
+{
+	v->visit(inSize);
+	v->visit(llength);
+}
+
+void OutputTimestep::accept(ASTVisitor* v)
+{
+	v->visit(outStep);
+}
+
+void HiddenSize::accept(ASTVisitor* v)
+{
+	v->visit(hSize);
+}
+
+void NumLayers::accept(ASTVisitor* v)
+{
+	v->visit(nLayers);
+}
+//!
+
+//! MLParams
+void InFeatures::accept(ASTVisitor* v)
+{
+	v->visit(inF);
+	v->visit(llength);
+}
+
+void OutFeatures::accept(ASTVisitor* v)
+{
+	for(auto &o : outFeat)
+		v->visit(o);
+}
+//!
+
+//! Expressions and Literals
+void BinExpr::accept(ASTVisitor* v)
+{
+	v->visit(lhs);
+	v->visit(rhs);
+}
+
+void LengthOf::accept(ASTVisitor* v)
+{
+	v->visit(layer);
+}
+//!
